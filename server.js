dfd52905730683b3c6d37024c155d8b090c28b60
Zac Edens <themancalledzac@gmail.com>
2021-03-16 12:50:16 -0700

    updated server.js file to include mongoose, middleware

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require("express");
-const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
+const mongoose = require("mongoose");
 
-require("./config/connect");
+// defines the middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// add routes, both API and view
+app.use(routes);
+
+// ??
+// require("./config/connect");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch");
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
