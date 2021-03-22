af057ac4e014fb6cdf16f4f1886a3d65e9530aad
Zac Edens <themancalledzac@gmail.com>
2021-03-22 12:00:16 -0700

    updating reducer actions

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,11 +1,22 @@
-// export const HOME_WEB_FULL = "HOME_WEB_FULL";
-// export const HOME_PHOTO_FULL = "HOME_PHOTO_FULL";
-// export const HOME_NORMAL = "HOME_NORMAL";
-
 // updated to redux style
 
 export const BOOK_SEARCH = () => {
   return {
     type: "BOOK_SEARCH",
+  };
+};
+export const BOOK_ADD = () => {
+  return {
+    type: "BOOK_ADD",
+  };
+};
+export const BOOK_REMOVE = () => {
+  return {
+    type: "BOOK_REMOVE",
+  };
+};
+export const BOOKS_SAVED = () => {
+  return {
+    type: "BOOKS_SAVED",
   };
 };
